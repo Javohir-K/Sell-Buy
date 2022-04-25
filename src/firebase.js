6a5db3f0e1ec18dedd402461a121f968f4b291c5
Javohir <61010500+Javohir-K@users.noreply.github.com>
2022-04-25 11:56:53 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2udnLz8jc24qNmh58Fmii9ntWg-gRjb4",
-  authDomain: "sell-buy-ed.firebaseapp.com",
-  projectId: "sell-buy-ed",
-  storageBucket: "sell-buy-ed.appspot.com",
-  messagingSenderId: "221365586291",
-  appId: "1:221365586291:web:bc6c13c5e26bd055a49d06",
-  measurementId: "G-4TBGXQEEN6",
+ // Firebase config code goes here
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
